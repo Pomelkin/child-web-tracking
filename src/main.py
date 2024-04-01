e3f4cd04b534740f77bbb05bb75316de2a8abcd0
Egorov E.A <emelya.kara@yandex.ru>
2024-04-02 00:43:03 +0500

    Add bruh

--- src/main.py
+++ src/main.py
@@ -38,8 +38,10 @@ app.add_middleware(
 
 @app.post("/frame", response_model=Stream)
 async def process_frame(stream: Stream):
-    data = base64.b64decode(stream.b64_frame)
-    frame = cv2.imdecode(np.frombuffer(data, np.uint8), cv2.IMREAD_COLOR)
+    data = stream.b64_frame
+    data_b64 = data.encode("utf-8")
+    bytes_data = base64.b64decode(data_b64)
+    frame = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
     cv2.rectangle(frame, (100, 100), (200, 200), (0, 255, 0), 2)
     _, img_encoded = cv2.imencode(".jpg", frame)
     encoded_frame = base64.b64encode(img_encoded).decode("utf-8")
