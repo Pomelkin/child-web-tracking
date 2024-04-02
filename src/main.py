2ebcfb2a5a74e8a64348177e674c83b082ae009c
Egorov E.A <emelya.kara@yandex.ru>
2024-04-02 16:47:35 +0500

    Add enhanced websockets

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import cv2
 import numpy as np
 import base64
+import asyncio
 from src.models import Stream
 
 app = FastAPI()
@@ -36,35 +37,52 @@ app.add_middleware(
 #             await connection.send_text(message)
 
 
-@app.post("/frame", response_model=Stream)
-async def process_frame(stream: Stream):
-    data = stream.b64_frame
-    data_b64 = data.encode("utf-8")
-    bytes_data = base64.b64decode(data_b64)
-    frame = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
-    cv2.rectangle(frame, (100, 100), (200, 200), (0, 255, 0), 2)
-    _, img_encoded = cv2.imencode(".jpg", frame)
-    encoded_frame = base64.b64encode(img_encoded).decode("utf-8")
-    return Stream(b64_frame=encoded_frame)
+# @app.post("/frame", response_model=Stream)
+# async def process_frame(stream: Stream):
+#     data = stream.b64_frame
+#     data_b64 = data.encode("utf-8")
+#     bytes_data = base64.b64decode(data_b64)
+#     frame = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+#     cv2.rectangle(frame, (100, 100), (200, 200), (0, 255, 0), 2)
+#     _, img_encoded = cv2.imencode(".jpg", frame)
+#     encoded_frame = base64.b64encode(img_encoded).decode("utf-8")
+#     return Stream(b64_frame=encoded_frame)
 
 
-# # manager = ConnectionManager()
-# active_connections = []
-#
-#
-# @app.websocket("/ws")
-# async def websocket_endpoint(websocket: WebSocket):
-#     await websocket.accept()
-#     active_connections.append(websocket)
-#     try:
-#         while True:
-#             data = await websocket.receive_text()
-#             data_b64 = data.encode("utf-8")
-#             img_data = base64.b64decode(data_b64)
-#             img = cv2.imdecode(np.frombuffer(img_data, np.uint8), cv2.IMREAD_COLOR)
-#             cv2.rectangle(img, (100, 100), (200, 200), (0, 255, 0), 2)
-#             _, img_encoded = cv2.imencode(".jpg", img)
-#             jpg_as_txt = base64.b64encode(img_encoded).decode("utf-8")
-#             await websocket.send_text(jpg_as_txt)
-#     except WebSocketDisconnect:
-#         active_connections.remove(websocket)
+# manager = ConnectionManager()
+active_connections = []
+active_tasks = []
+
+
+async def receive(websocket: WebSocket, queue: asyncio.Queue):
+    base64_img = await websocket.receive_text()
+    try:
+        queue.put_nowait(base64_img)
+    except asyncio.QueueFull:
+        pass
+
+
+async def detect(websocket: WebSocket, queue: asyncio.Queue):
+    while True:
+        base64_img = await queue.get()
+        bytes_img = base64.b64decode(base64_img)
+        arr_img = np.frombuffer(bytes_img, dtype=np.uint8)
+        img = cv2.imdecode(arr_img, cv2.IMREAD_COLOR)
+        cv2.rectangle(img, (100, 100), (200, 200), (0, 255, 0), 2)
+        await websocket.send_json({"bboxes": [100, 100, 200, 200]})
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    active_connections.append(websocket)
+    queue = asyncio.Queue()
+    detect_task = asyncio.create_task(detect(websocket, queue))
+    active_tasks.append(detect_task)
+    try:
+        while True:
+            await receive(websocket, queue)
+    except WebSocketDisconnect:
+        active_connections.remove(websocket)
+        active_tasks.remove(detect_task)
+        queue.put_nowait(None)
