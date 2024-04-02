c7372483ffbaa7f77ab20eba2552a51732fdef07
Egorov E.A <emelya.kara@yandex.ru>
2024-04-02 20:37:25 +0500

    Fix path to main for uvicorn

--- src/main.py
+++ src/main.py
@@ -100,4 +100,4 @@ async def websocket_endpoint(websocket: WebSocket):
 
 
 if __name__ == "__main__":
-    uvicorn.run("src.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
