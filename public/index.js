629cc6d5c76bff4e0fafa4427c8af57353b587e0
Sh6abrA <andpro22223@gmail.com>
2024-04-02 17:00:40 +0500

    Add huyna

--- public/index.js
+++ public/index.js
@@ -3,7 +3,7 @@ const outCtx = output.getContext('2d');
 const videoCanvas = document.getElementById('video');
 const ctx = videoCanvas.getContext('2d');
 const input = document.getElementById('input');
-// const ws = new WebSocket('ws://ontollm.semograph.com:28080/ws');
+const ws = new WebSocket('ws://ontollm.semograph.com:28080/ws');
 videoCanvas.width = 640;
 videoCanvas.height = 480;
 output.width = 640;
@@ -19,30 +19,26 @@ function onVideo(){
     setInterval(() => {
         ctx.drawImage(input, 0, 0, videoCanvas.width, videoCanvas.height);
         captureFrame();
-    }, 500)
+    }, 100)
 }
 function captureFrame () {
     const imageData = ctx.getImageData(0, 0, videoCanvas.width, videoCanvas.height);
     const dataURL = videoCanvas.toDataURL().split(',')[1];
-    axios.post('http://ontollm.semograph.com:28080/frame', { b64_frame: dataURL})
-    .then(res => {
-        drawImage(res.data.b64_frame);
-    })
+    ws.send(dataURL);
 }
 
-// ws.onmessage = (event) => {
-//     const base64 = 'data:image/jpeg;base64,' + event.data;
-//     const img = new Image();
-//     img.src = base64;
-//     img.onload = () => {
-//         outCtx.drawImage(img, 0, 0, output.width, output.height);
-//     }
-// }
-function drawImage(base64){
-    const newbase64 = 'data:image/jpeg;base64,' + base64;
-    const img = new Image();
-    img.src = newbase64;
-    img.onload = () => {
-        outCtx.drawImage(img, 0, 0, output.width, output.height);
-    }
+ws.onmessage = (event) => {
+    drawImage(event.data);
+}
+function drawImage(data){
+    console.log(data)
+    const bbox =  JSON.parse(data).bboxes;
+    console.log(bbox)
+    outCtx.drawImage(input, 0, 0, videoCanvas.width, videoCanvas.height);
+    outCtx.beginPath();
+    outCtx.lineWidth = "2";
+    outCtx.strokeStyle = 'red';
+    outCtx.rect(bbox[0], bbox[1], bbox[2], bbox[3]);
+    outCtx.stroke();
+    
 }
