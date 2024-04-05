ee96d8d83a037a5d8f5b92317cc897b82da8dfae
Egorov E.A <emelya.kara@yandex.ru>
2024-04-05 18:41:05 +0500

    Run YOLO in child process

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from asyncio import AbstractEventLoop
 from contextlib import asynccontextmanager
-from asyncio import Lock
+
+# from asyncio import Lock
 import uvicorn
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
@@ -9,6 +10,7 @@ import numpy as np
 import base64
 import asyncio
 import multiprocessing as mp
+from multiprocessing import Lock
 from concurrent.futures import ProcessPoolExecutor
 from ultralytics import YOLO
 from src.models import Stream
@@ -22,8 +24,9 @@ shared_values = {}
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Load the ML model
+    manager = mp.Manager()
     ml_models["yolo"] = YOLO("yolov8n.pt")
-    shared_values["lock"] = Lock()
+    shared_values["lock"] = manager.Lock()
     yield
     # Clean up the ML models and release the resources
     ml_models.clear()
@@ -34,33 +37,33 @@ app = FastAPI(lifespan=lifespan)
 active_connections = []
 
 
-def process_img(base64_img: str) -> np.ndarray:
+def process_img(base64_img: str, model: YOLO, lock: Lock) -> list[list[int]]:
     print(torch.cuda.is_available())
     bytes_img = base64.b64decode(base64_img)
     arr_img = np.frombuffer(bytes_img, dtype=np.uint8)
     img = cv2.imdecode(arr_img, cv2.IMREAD_COLOR)
-    return img
+
+    with lock:
+        results = model.predict(img)
+
+    boxes = []
+    for bboxes in results[0].boxes.data.tolist():
+        x1, y1, x2, y2, _, _ = bboxes
+        boxes.append([x1, y1, x2, y2])
+    return boxes
 
 
 async def detection(websocket: WebSocket, model: YOLO, lock: Lock):
     assert id(lock) == id(shared_values["lock"])
     assert id(model) == id(ml_models["yolo"])
 
-    with ProcessPoolExecutor() as pool:
+    with ProcessPoolExecutor(max_workers=1) as pool:
         loop: AbstractEventLoop = asyncio.get_event_loop()
         while True:
             base64_img = await websocket.receive_text()
-            get_img = loop.run_in_executor(pool, process_img, base64_img)
-            img = await get_img
-
-            async with shared_values["lock"]:
-                results = model.predict(img)
-
-            boxes = []
-            for bboxes in results[0].boxes.data.tolist():
-                x1, y1, x2, y2, _, _ = bboxes
-                boxes.append([x1, y1, x2, y2])
-                print(x1, y1, x2, y2)
+            boxes = await loop.run_in_executor(
+                pool, process_img, base64_img, model, lock
+            )
 
             await websocket.send_json({"bboxes": boxes})
 
